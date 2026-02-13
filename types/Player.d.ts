8a4504f7823e7b2fde9f0f95e08c298478941a66
Rendall <rendall@gmail.com>
2026-02-13 12:02:49 +0200

    types: tighten unknowns in Player.d.ts

--- types/Player.d.ts
+++ types/Player.d.ts
@@ -11,25 +11,26 @@ export = Player;
  * @extends Character
  */
 declare class Player extends Character {
-    account: unknown;
-    experience: unknown;
-    extraPrompts: Map<unknown, unknown>;
-    password: unknown;
-    prompt: unknown;
-    socket: unknown;
+    constructor(data: PlayerData);
+    account: Account | string | null;
+    experience: number;
+    extraPrompts: Map<string, PlayerExtraPrompt>;
+    password: string;
+    prompt: string;
+    socket: import("./Broadcastable").SocketLike | null;
     questTracker: QuestTracker;
     commandQueue: CommandQueue;
-    role: unknown;
+    role: PlayerRole;
     /**
      * @see CommandQueue::enqueue
      */
-    queueCommand(executable: unknown, lag: unknown): void;
+    queueCommand(executable: PlayerCommandExecutable, lag: number): void;
     /**
      * Convert prompt tokens into actual data
      * @param {string} promptStr
      * @param {object} extraData Any extra data to give the prompt access to
      */
-    interpolatePrompt(promptStr: string, extraData?: object): string;
+    interpolatePrompt(promptStr: string, extraData?: Record<string, unknown>): string;
     /**
      * Add a line of text to be displayed immediately after the prompt when the prompt is displayed
      * @param {string}      id       Unique prompt id
@@ -37,7 +38,7 @@ declare class Player extends Character {
      * @param {?boolean}    removeOnRender When true prompt will remove itself once rendered
      *    otherwise prompt will continue to be rendered until removed.
      */
-    addPrompt(id: string, renderer: () => unknown, removeOnRender?: boolean | null): void;
+    addPrompt(id: string, renderer: () => string, removeOnRender?: boolean | null): void;
     /**
      * @param {string} id
      */
@@ -55,11 +56,36 @@ declare class Player extends Character {
      * @fires Room#playerEnter
      * @fires Player#enterRoom
      */
-    moveTo(nextRoom: Room, onMoved?: Function): void;
-    save(callback: unknown): void;
-    hydrate(state: unknown): void;
+    moveTo(nextRoom: Room, onMoved?: () => unknown): void;
+    save(callback?: unknown): void;
+    hydrate(state: GameState): void;
 }
+import Account = require("./Account");
 import Character = require("./Character");
 import QuestTracker = require("./QuestTracker");
 import CommandQueue = require("./CommandQueue");
 import Room = require("./Room");
+import PlayerRoles = require("./PlayerRoles");
+import GameState = require("./GameState");
+type PlayerRole = typeof PlayerRoles[keyof typeof PlayerRoles];
+type PlayerExtraPrompt = {
+    removeOnRender: boolean;
+    renderer: () => string;
+};
+type PlayerCommandExecutable = {
+    execute: () => void;
+    label: string;
+    lag?: number;
+};
+type PlayerData = ConstructorParameters<typeof Character>[0] & {
+    account?: Account | string | null;
+    experience?: number;
+    password: string;
+    prompt?: string;
+    socket?: import("./Broadcastable").SocketLike | null;
+    quests?: {
+        completed?: string[];
+        active?: string[];
+    };
+    role?: PlayerRole;
+};
