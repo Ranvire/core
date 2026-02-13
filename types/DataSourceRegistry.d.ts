c531d4c084d1d49ede79eb8ebfebb92ee604927e
Rendall <rendall@gmail.com>
2026-02-13 11:53:11 +0200

    types: tighten unknowns in DataSourceRegistry.d.ts

--- types/DataSourceRegistry.d.ts
+++ types/DataSourceRegistry.d.ts
@@ -3,15 +3,24 @@ export = DataSourceRegistry;
  * Holds instances of configured DataSources
  * @type {Map<string, DataSource>}
  */
-declare class DataSourceRegistry extends Map<unknown, unknown> {
+declare class DataSourceRegistry extends Map<string, DataSource> {
     constructor();
-    constructor(entries?: readonly (readonly [unknown, unknown])[]);
-    constructor();
-    constructor(iterable?: Iterable<readonly [unknown, unknown]>);
+    constructor(entries?: readonly (readonly [string, DataSource])[]);
+    constructor(iterable?: Iterable<readonly [string, DataSource]>);
     /**
      * @param {Function} requireFn used to require() the loader
      * @param {string} rootPath project root
      * @param {object} config configuration to load
      */
-    load(requireFn: Function, rootPath: string, config?: object): void;
+    load(requireFn: (moduleName: string) => unknown, rootPath: string, config?: Record<string, DataSourceLoaderConfig>): void;
 }
+type DataSource = {
+    name: string;
+    hasData(...args: unknown[]): unknown;
+    [key: string]: unknown;
+};
+type DataSourceLoaderConfig = {
+    require: string;
+    config?: Record<string, unknown>;
+    [key: string]: unknown;
+};
