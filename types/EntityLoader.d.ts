70ca083546e595c2fbbe4fceecfefe462ec4bbd1
Rendall <rendall@gmail.com>
2026-02-13 11:56:29 +0200

    types: tighten unknowns in EntityLoader.d.ts

--- types/EntityLoader.d.ts
+++ types/EntityLoader.d.ts
@@ -7,14 +7,28 @@ declare class EntityLoader {
      * @param {DataSource}
      * @param {object} config
      */
-    constructor(dataSource: unknown, config?: object);
-    dataSource: unknown;
-    config: unknown;
-    setArea(name: unknown): void;
-    setBundle(name: unknown): void;
-    hasData(): unknown;
+    constructor(dataSource: EntityDataSource, config?: EntityLoaderConfig);
+    dataSource: EntityDataSource;
+    config: EntityLoaderConfig;
+    setArea(name: string): void;
+    setBundle(name: string): void;
+    hasData(): boolean | Promise<boolean>;
+    // Concrete payload shape depends on the backing data source.
     fetchAll(): unknown;
     fetch(id: unknown): unknown;
     replace(data: unknown): unknown;
     update(id: unknown, data: unknown): unknown;
 }
+type EntityLoaderConfig = {
+    area?: string;
+    bundle?: string;
+    [key: string]: unknown;
+};
+type EntityDataSource = {
+    name?: string;
+    hasData(config: EntityLoaderConfig): boolean | Promise<boolean>;
+    fetchAll?(config: EntityLoaderConfig): unknown;
+    fetch?(config: EntityLoaderConfig, id: unknown): unknown;
+    replace?(config: EntityLoaderConfig, data: unknown): unknown;
+    update?(config: EntityLoaderConfig, id: unknown, data: unknown): unknown;
+};
