df73186dad5edf5d5e1346ce75c819e6efa1b49e
Rendall <rendall@gmail.com>
2026-02-13 12:00:26 +0200

    types: tighten unknowns in Logger.d.ts

--- types/Logger.d.ts
+++ types/Logger.d.ts
@@ -4,12 +4,12 @@ export = Logger;
  */
 declare class Logger {
     static getLevel(): string;
-    static setLevel(level: unknown): void;
+    static setLevel(level: string): void;
     static log(...messages: unknown[]): void;
     static error(...messages: unknown[]): void;
     static warn(...messages: unknown[]): void;
     static verbose(...messages: unknown[]): void;
-    static setFileLogging(path: unknown): void;
+    static setFileLogging(path: string): void;
     static deactivateFileLogging(): void;
     static enablePrettyErrors(): void;
 }
