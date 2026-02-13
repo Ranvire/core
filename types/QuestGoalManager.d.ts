13149618dee090240c74d125924a309f911d3cf9
Rendall <rendall@gmail.com>
2026-02-13 12:04:49 +0200

    types: tighten unknowns in QuestGoalManager.d.ts

--- types/QuestGoalManager.d.ts
+++ types/QuestGoalManager.d.ts
@@ -2,9 +2,12 @@ export = QuestGoalManager;
 /**
  * Simple map of quest goal name => class definition
  */
-declare class QuestGoalManager extends Map<unknown, unknown> {
+declare class QuestGoalManager extends Map<string, QuestGoalType> {
     constructor();
-    constructor(entries?: readonly (readonly [unknown, unknown])[]);
-    constructor();
-    constructor(iterable?: Iterable<readonly [unknown, unknown]>);
+    constructor(entries?: readonly (readonly [string, QuestGoalType])[]);
+    constructor(iterable?: Iterable<readonly [string, QuestGoalType]>);
 }
+import Quest = require("./Quest");
+import Player = require("./Player");
+import QuestGoal = require("./QuestGoal");
+type QuestGoalType = new (quest: Quest, config: Record<string, unknown>, player: Player) => QuestGoal;
