0ba9ef55f44b014b904208134a6922b239916cbc
Rendall <rendall@gmail.com>
2026-02-13 12:07:11 +0200

    types: tighten unknowns in Scriptable.d.ts

--- types/Scriptable.d.ts
+++ types/Scriptable.d.ts
@@ -5,4 +5,17 @@ export = Scriptable;
  * @param {*} parentClass
  * @return {module:ScriptableFn~Scriptable}
  */
-declare function Scriptable(parentClass: unknown): unknown;
+declare function Scriptable<TBase extends Constructor<ScriptableBase>>(parentClass: TBase): TBase & Constructor<ScriptableInstance>;
+import BehaviorManager = require("./BehaviorManager");
+type Constructor<T = object> = new (...args: unknown[]) => T;
+type ScriptableBase = {
+    behaviors: Map<string, unknown>;
+    __pruned?: boolean;
+    emit(name: string, ...args: unknown[]): unknown;
+    removeAllListeners(): void;
+};
+type ScriptableInstance = {
+    hasBehavior(name: string): boolean;
+    getBehavior(name: string): unknown;
+    setupBehaviors(manager: BehaviorManager): void;
+};
