e075dce9759206cc51f3a14aa2dad8bb0919447a
Rendall <rendall@gmail.com>
2026-02-13 12:04:12 +0200

    types: tighten unknowns in QuestFactory.d.ts

--- types/QuestFactory.d.ts
+++ types/QuestFactory.d.ts
@@ -3,22 +3,22 @@ export = QuestFactory;
  * @property {Map} quests
  */
 declare class QuestFactory {
-    quests: Map<unknown, unknown>;
-    add(areaName: unknown, id: unknown, config: unknown): void;
-    set(qid: unknown, val: unknown): void;
+    quests: Map<entityReference, QuestFactoryDefinition>;
+    add(areaName: string, id: string | number, config: QuestConfig): void;
+    set(qid: entityReference, val: QuestFactoryDefinition): void;
     /**
      * Get a quest definition. Use `create` if you want an instance of a quest
      * @param {string} qid
      * @return {object}
      */
-    get(qid: string): object;
+    get(qid: string): QuestFactoryDefinition | undefined;
     /**
      * Check to see if a player can start a given quest based on the quest's
      * prerequisite quests
      * @param {entityReference} questRef
      * @return {boolean}
      */
-    canStart(player: unknown, questRef: entityReference): boolean;
+    canStart(player: Player, questRef: entityReference): boolean;
     /**
      * @param {GameState} GameState
      * @param {entityReference} qid
@@ -26,14 +26,26 @@ declare class QuestFactory {
      * @param {Array}     state     current quest state
      * @return {Quest}
      */
-    create(GameState: unknown, qid: entityReference, player: Player, state?: unknown[]): Quest;
+    create(GameState: GameState, qid: entityReference, player: Player, state?: QuestGoalState[]): Quest;
     /**
      * @param {string} areaName
      * @param {number} id
      * @return {string}
      */
-    makeQuestKey(area: unknown, id: number): string;
+    makeQuestKey(area: string, id: number): string;
 }
+import GameState = require("./GameState");
 import Quest = require("./Quest");
 import Player = require("./Player");
-type entityReference = unknown;
+type entityReference = string;
+type QuestConfig = ConstructorParameters<typeof Quest>[2];
+type QuestGoalState = ConstructorParameters<typeof Quest>[0] extends never ? never : {
+    state: Record<string, unknown>;
+    [key: string]: unknown;
+};
+type QuestFactoryDefinition = {
+    id: string | number;
+    area: string;
+    config: QuestConfig;
+    npc?: string;
+};
