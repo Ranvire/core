5334d5c9c7c03693697db039bc4f07b541575261
Rendall <rendall@gmail.com>
2026-02-13 12:13:29 +0200

    types: tighten unknowns in EntityLoaderRegistry.d.ts

--- types/EntityLoaderRegistry.d.ts
+++ types/EntityLoaderRegistry.d.ts
@@ -8,11 +8,15 @@ declare class EntityLoaderRegistry extends Map<string, EntityLoader> {
     constructor(entries?: readonly (readonly [string, EntityLoader])[]);
     constructor(iterable?: Iterable<readonly [string, EntityLoader]>);
     load(sourceRegistry: {
-        get(name: string): unknown;
+        get(name: string): EntityDataSourceLike | undefined;
     }, config?: Record<string, EntityLoaderDefinition>): void;
 }
 import EntityLoader = require("./EntityLoader");
 type EntityLoaderDefinition = {
     source: string;
     config?: Record<string, unknown>;
+};
+type EntityDataSourceLike = {
+    hasData(config: Record<string, unknown>): boolean | Promise<boolean>;
+    [key: string]: unknown;
 };
