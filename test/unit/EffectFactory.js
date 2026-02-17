88256dea70c5225147549988166c7d0c7b2078a0
Rendall <rendall@gmail.com>
2026-02-17 12:51:51 +0200

    Add tests

--- test/unit/EffectFactory.js
+++ test/unit/EffectFactory.js
@@ -20,4 +20,36 @@ describe('EffectFactory', () => {
       });
     });
   });
+
+  describe('#create', () => {
+    it('does not leak config overrides into subsequent instances', () => {
+      const factory = new EffectFactory();
+      factory.add('burn', {
+        config: { name: 'Burning', potency: 5 },
+        state: { stacks: 1 },
+        listeners: {},
+      });
+
+      const first = factory.create('burn', { potency: 99 });
+      assert.strictEqual(first.config.potency, 99);
+
+      const second = factory.create('burn');
+      assert.strictEqual(second.config.potency, 5);
+    });
+
+    it('does not leak state overrides into subsequent instances', () => {
+      const factory = new EffectFactory();
+      factory.add('burn', {
+        config: { name: 'Burning' },
+        state: { stacks: 1 },
+        listeners: {},
+      });
+
+      const first = factory.create('burn', {}, { stacks: 7 });
+      assert.strictEqual(first.state.stacks, 7);
+
+      const second = factory.create('burn');
+      assert.strictEqual(second.state.stacks, 1);
+    });
+  });
 });
